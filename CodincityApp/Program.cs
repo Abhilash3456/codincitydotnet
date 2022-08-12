db208a5440dee284534669c320d77b8872a70d62
Abhilash3456 <abhilash.subramanian@codincity.com>
2022-08-12 05:29:01 +0000

    first commit

---
+++ CodincityApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodincityApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Codiners this is our first dotnet project");
+        }
+    }
+}
